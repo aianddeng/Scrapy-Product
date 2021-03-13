5e4873f74cbf51a60cdb4e3a6934b622226d0fd3
Chase <aianddeng@gmail.com>
2021-03-13 09:39:29 +0800

    fix time.

--- src/main.js
+++ src/main.js
@@ -268,9 +268,9 @@ const main = async targets => {
             );
 
             // 等待1秒
-            const uiTask = taskList.map(el => el.url).filter(url => url.match(/^https:\/\/www\.ebay\.com\/(b|sch)\/(.+)/));
-            
-            await new Promise(resolve => setTimeout(resolve, 0.6 * 1000 + (uiTask.length * 1.5)));
+            const uiTask = currentList.map(el => el.url).filter(url => url.match(/^https:\/\/www\.ebay\.com\/(b|sch)\/(.+)/));
+
+            await new Promise(resolve => setTimeout(resolve, 0.6 * 1000 + (uiTask.length * 1.5) * 1000));
         }
     }
 }
